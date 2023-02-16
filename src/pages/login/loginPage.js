ab839b834f62bdc2972936442b179aea81ee0735
prclima <pedro@futuranet.com.br>
2023-02-16 18:04:07 -0300

    desktop done

--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -3,26 +3,32 @@ import login_img from "../../images/login-img.png";
 
 function Login() {
   return (
-    <div className={style.wrapper}>
-      <div className={style.loginBox}>
-        <div className={style.loginImage}>
-          <img src={login_img} alt="login" />
-        </div>
+    <>
+      <div className={style.wrapper}>
+        <div className={style.loginBox}>
+          <div className={style.loginImage}>
+            <img src={login_img} alt="login" />
+          </div>
 
-        <div className={style.loginInput}>
-          <h2>Login Title</h2>
-          <label>User Name</label>
-          <input />
+          <div className={style.loginInput}>
+            <h2 className="title">Welcome</h2>
+            <label className="label">User Name</label>
+            <input className="input" type={"text"} />
 
-          <label>Password</label>
-          <input />
+            <label className="label">Password</label>
+            <input className="input" type="password" />
 
-          <button>Go</button>
+            <button className="button">SUBMIT</button>
 
-          <a href="###">Forgot my password</a>
+            <div className={style.check}>
+              <input type="checkbox" id="remember" name="remember" />
+              <label htmlFor="remember">Remember me</label>
+              <a href="###">Forgot password?</a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
